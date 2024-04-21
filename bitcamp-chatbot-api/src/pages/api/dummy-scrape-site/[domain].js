fd47df4119fa12ff53e5c9fbd3082d8f0dad2e60
Vikram Khandelwal <vikram.a.khandelwal@gmail.com>
2024-04-20 22:22:04 -0400

    thing

--- bitcamp-chatbot-api/src/pages/api/dummy-scrape-site/[domain].js
+++ bitcamp-chatbot-api/src/pages/api/dummy-scrape-site/[domain].js
@@ -1,3 +1,7 @@
 export default function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Adjust this to the correct domain
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS'); // Optionally add POST, PUT, DELETE, etc.
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
     res.status(200).json({ message: 'asst_Ttw0tknAKTL1oR5bJpxOrEfF' });
 }
